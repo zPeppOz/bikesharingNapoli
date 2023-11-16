783f6da1cc4e429eecb1e987ee12bfd59788f317
Giuseppe Setaro <giuseppe.setaro001@studenti.uniparthenope.it>
2023-11-16 16:35:38 +0100

    init

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./home/Home";
 import About from "./About";
+import MainApp from "./app/MainApp";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="/app" element={<MainApp />} />
     </Routes>
   );
 }
---
+++ src/app/MainApp.jsx
@@ -0,0 +1,3 @@
+export default function MainApp(props) {
+  return <p>Test</p>;
+}
