550350c48c218b876cb3a3645a6e267f7e8935a2
prisco <prisco.sorgente001@studenti.uniparthenope.it>
2023-12-14 13:00:34 +0100

    dashboard wip

--- src/dashboard/components/DataGridComponent.jsx
+++ src/dashboard/components/DataGridComponent.jsx
@@ -1,17 +1,40 @@
 import React, { useState } from "react";
 import { DataGrid } from "@mui/x-data-grid";
-import { Button } from "@mui/material";
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  TextField,
+  DialogActions,
+} from "@mui/material";
 
 export default function DataGridComponent(props) {
   const [rows, setRows] = useState(props.rows);
+  const [isDialogOpen, setDialogOpen] = useState(false);
+
+  // // Ho inizializzato newRowData come un oggetto vuoto ({}) perché non so quali colonne ci saranno.
+  // In questo modo, possiamo utilizzare il componente per diverse colonne dinamicamente.
+  const [newRowData, setNewRowData] = useState({});
 
   const handleAddRow = () => {
-    // Aggiungi una nuova riga
-    const newRows = [
-      ...rows,
-      { id: rows.length + 1, Bicicleta: "", data: "", descrizione: "" },
-    ];
+    // Apri la finestra modale per l'inserimento dei dati
+    setDialogOpen(true);
+  };
+
+  const handleSaveRow = () => {
+    // Chiudi la finestra modale e aggiungi la nuova riga
+    const newRows = [...rows, { id: rows.length + 1, ...newRowData }];
     setRows(newRows);
+    setDialogOpen(false);
+    // Resettare i dati del nuovo inserimento
+    setNewRowData({});
+  };
+
+  // questa funzione in modo che sia in grado di gestire qualsiasi
+  //  campo di input in base alle colonne passate come props.
+  const handleInputChange = (e) => {
+    // Aggiorna lo stato con i dati del nuovo inserimento
+    setNewRowData({ ...newRowData, [e.target.name]: e.target.value });
   };
 
   return (
@@ -21,6 +44,35 @@ export default function DataGridComponent(props) {
           Aggiungi Riga
         </Button>
       </div>
+
+      <Dialog open={isDialogOpen} onClose={() => setDialogOpen(false)}>
+        <DialogContent>
+          <div
+            className="flex flex-col items-center justify-center"
+            style={{
+              gap: "0.75rem",
+            }}
+          >
+            {props.columns.map((column) => (
+              <TextField
+                key={column.field}
+                label={column.headerName}
+                name={column.field}
+                value={newRowData[column.field] || ""}
+                onChange={handleInputChange}
+                fullWidth
+              />
+            ))}
+          </div>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setDialogOpen(false)}>Annulla</Button>
+          <Button variant="contained" onClick={handleSaveRow}>
+            Salva
+          </Button>
+        </DialogActions>
+      </Dialog>
+
       <div style={{ height: 400, width: "100%" }}>
         <DataGrid
           rows={rows}
