5254dc673bba085f5e2d5150111f6c881b8f37b0
Giuseppe Setaro <giuseppe.setaro001@studenti.uniparthenope.it>
2024-02-20 09:38:51 +0100

    user location

--- src/app/components/Map.jsx
+++ src/app/components/Map.jsx
@@ -15,7 +15,9 @@ import MapMarker from "./Marker";
 
 export default function Map({ biciclette, stazioni, handlers }) {
   const [isLoading, setIsLoading] = useState(true);
+  const [userLocation, setUserLocation] = useState(null);
   const map = useMap();
+
   const events = useMapEvents({
     load: (e) => {
       !isLoading && map.locate();
@@ -26,6 +28,15 @@ export default function Map({ biciclette, stazioni, handlers }) {
       handlers.toggleMenu(false);
     },
   });
+
+  useEffect(() => {
+    if (!isLoading) {
+      map.locate({ setView: true, maxZoom: 16 }).on("locationfound", (e) => {
+        setUserLocation(e.latlng); // Aggiorna lo stato con la posizione dell'utente
+        map.flyTo(e.latlng, 16); // Zooma sulla posizione dell'utente
+      });
+    }
+  }, [isLoading, map]);
 
   useEffect(() => {
     if (biciclette && stazioni) {
@@ -40,6 +51,9 @@ export default function Map({ biciclette, stazioni, handlers }) {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         zIndex={0}
       />
+      {userLocation && (
+        <Marker position={userLocation} icon={userLocationIcon}></Marker>
+      )}
       {stazioni.map((stazione) => {
         return (
           <MapMarker
@@ -94,4 +108,10 @@ const stationIcon = L.divIcon({
   html: '<img src="' + station + '"  />',
   iconSize: [40, 40],
   className: "border rounded-full p-1 border-black !w-12 !h-12 bg-green-300 ",
+});
+
+const userLocationIcon = L.divIcon({
+  html: '<div style="background-color: #007bff; width: 12px; height: 12px; border-radius: 50%; border: 2px solid white;"></div>',
+  className: "",
+  iconSize: [12, 12],
 });
